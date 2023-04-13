7fbb8f50750c8b3337034bf91a94618ebcbc6e7f
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2023-04-13 22:49:53 +0300

    js hw 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMackup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryItemClick);
+
+function createGalleryMackup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+    })
+    .join('');
+}
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+  const isGalleryImageEl = evt.target.nodeName;
+
+  if (isGalleryImageEl !== 'IMG') {
+    return;
+  }
+
+  const largeImageSrc = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+      <img src="${largeImageSrc}" width="1100" height="800">
+  `);
+
+  instance.show();
+
+  document.addEventListener('keydown', closeModalWindow);
+
+  function closeModalWindow(evt) {
+    if (evt.keyCode === 27) {
+      instance.close();
+    }
+  }
+
+  instance.element().addEventListener('click', () => {
+    instance.close();
+    document.removeEventListener('keydown', closeModalWindow);
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMackup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryItemClick);
+
+function createGalleryMackup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+        </a>
+      </li>
+    `;
+    })
+    .join('');
+}
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery .gallery__link', {
+        captionsData: 'alt',
+        captionDelay: 250,
+  });
+}
